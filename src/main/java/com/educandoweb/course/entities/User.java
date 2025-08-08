be2d7eacd7869b7ffd56d93e36e28d1708ef15e5
israel.paixao <israeldouglas25@gmail.com>
2025-08-08 15:54:49 -0300

    create H2 database, JPA, application-test

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,9 +1,14 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -11,6 +16,7 @@ import java.util.UUID;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private String name;
